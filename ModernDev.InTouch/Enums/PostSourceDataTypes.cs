3c966541acaa0acde2a579f85d5760630404d9f8
Bohdan Shtepan <winsatana@ya.ru>
2016-03-02 08:34:41 +0200

    Add PostSourceDataTypes, PostSourcePlatforms, PostSourceTypes and PostTypes enums.

---
+++ ModernDev.InTouch/Enums/PostSourceDataTypes.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Runtime.Serialization;
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// Action types due to which the record was created.
+    /// </summary>
+    public enum PostSourceDataTypes
+    {
+        // if type = vk
+
+        /// <summary>
+        /// Change of user's status.
+        /// </summary>
+        [EnumMember(Value = "profile_activity")]
+        ProfileActivity,
+
+        /// <summary>
+        /// Change of user's photo.
+        /// </summary>
+        [EnumMember(Value = "profile_photo")]
+        ProfilePhoto,
+
+        // if type = widget
+
+        /// <summary>
+        /// Comments widget.
+        /// </summary>
+        [EnumMember(Value = "comments")]
+        Comments,
+
+        /// <summary>
+        /// Likes widget.
+        /// </summary>
+        [EnumMember(Value = "like")]
+        Like,
+
+        /// <summary>
+        /// Poll widget.
+        /// </summary>
+        [EnumMember(Value = "poll")]
+        Poll
+    }
+}
