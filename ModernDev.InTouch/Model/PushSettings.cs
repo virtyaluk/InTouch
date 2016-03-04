8d5969e0dd5dd0bca586a058315c5451bdfcff39
Bohdan Shtepan <winsatana@ya.ru>
2016-03-04 02:12:44 +0200

    Add PushSettings class.

---
+++ ModernDev.InTouch/Model/PushSettings.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// Chat notification settings.
+    /// </summary>
+    [DataContract]
+    [DebuggerDisplay("PushSettings")]
+    public class PushSettings
+    {
+        /// <summary>
+        /// Sounds status.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("sounds")]
+        public int Sounds { get; set; }
+
+        /// <summary>
+        /// Disabled status.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("disabled_until")]
+        public int DisabledUntil { get; set; }
+    }
+}
