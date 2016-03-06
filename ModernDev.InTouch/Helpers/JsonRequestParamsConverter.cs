f6a38ed07acdec7e1ced28f63cd8f757417f32c1
Bohdan Shtepan <winsatana@ya.ru>
2016-03-06 18:49:34 +0200

    Add JsonRequestParamsConverter class.

---
+++ ModernDev.InTouch/Helpers/JsonRequestParamsConverter.cs
@@ -0,0 +1,39 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ModernDev.InTouch.API
+{
+    public class JsonRequestParamsConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteNull();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return serializer.Deserialize<JArray>(reader)
+                .OfType<JObject>()
+                .ToDictionary(jObj => (string) jObj["key"], jObj => (string) jObj["value"]);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
