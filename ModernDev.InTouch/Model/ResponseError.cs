990d5e8dc8ae452d854bcab71c4da1845684f920
Bohdan Shtepan <winsatana@ya.ru>
2016-03-06 22:31:02 +0200

    Add ResponseError class.

---
+++ ModernDev.InTouch/Model/ResponseError.cs
@@ -0,0 +1,63 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ModernDev.InTouch.API
+{
+    /// <summary>
+    /// A basic class for response error.
+    /// </summary>
+    [DebuggerDisplay("ResponseError")]
+    [DataContract]
+    public class ResponseError
+    {
+        /// <summary>
+        /// Error code.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("error_code")]
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Error text.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("error_msg")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Captcha identifier.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("captcha_sid")]
+        public string CaptchaSId { get; set; }
+
+        /// <summary>
+        /// A link to an image that will be shown to a user.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("captcha_img")]
+        public string CaptchaImg { get; set; }
+
+        /// <summary>
+        /// Request parameters.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("request_params")]
+        [JsonConverter(typeof(JsonRequestParamsConverter))]
+        public Dictionary<string, string> RequestParams { get; set; } 
+    }
+}
