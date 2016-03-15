08b0d98759367299df2f32b4d3e07f9f062cb031
Bohdan Shtepan <winsatana@ya.ru>
2016-03-15 02:01:26 +0200

    Add UserSubscriptios and UserSubscriptionsExtended classes.

---
+++ ModernDev.InTouch/Model/UserSubscriptionsExtended.cs
@@ -0,0 +1,32 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// User subscriptions extended.
+    /// </summary>
+    [DebuggerDisplay("UserSubscriptionsExtended")]
+    [DataContract]
+    public class UserSubscriptionsExtended : ItemsList<IProfileItem>
+    {
+        [DataMember]
+        [JsonProperty("items")]
+        [JsonConverter(typeof (JsonIProfileItemListConverter))]
+        public new List<IProfileItem> Items { get; set; }
+    }
+}
