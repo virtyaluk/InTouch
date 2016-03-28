2747d1168a56278f47d55002d9ab09a65032ece2
Bohdan Shtepan <winsatana@ya.ru>
2016-03-28 21:30:29 +0300

    Add MethodParamsGroup class.

---
+++ ModernDev.InTouch.Shared/API/MethodParamsGroup.cs
@@ -0,0 +1,62 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// A base class for all method parameters classes.
+    /// </summary>
+    public abstract class MethodParamsGroup
+    {
+        protected InTouch API;
+
+        /// <summary>
+        /// Iterates over all non-nullable class properties and adds them to the <see cref="MethodParamsGroup"/> collection.
+        /// </summary>
+        /// <returns>A <see cref="MethodParamsGroup"/> instance containing all the non-nullable properties of the current class.</returns>
+        public MethodParams GetParams()
+        {
+            var mp = new MethodParams();
+
+#if WINDOWS_UWP81
+            var properties = GetType().GetTypeInfo().DeclaredProperties.Where(propInfo => propInfo.CanRead);
+#else
+            var properties = GetType().GetProperties(BindingFlags.Public).Where(prop => prop.CanRead);
+#endif
+
+            foreach (var prop in properties)
+            {
+                var val = prop.GetValue(this);
+
+                if (val != null)
+                {
+                    var mpa = prop.GetCustomAttribute<MethodParamAttribute>(true);
+
+                    mp.Add(mpa.Name, val, mpa.IsRequired, mpa.Extra);
+                }
+            }
+
+            return mp;
+        }
+
+        public MethodParams GetParams(InTouch api)
+        {
+            API = api;
+
+            return GetParams();
+        }
+    }
+}
