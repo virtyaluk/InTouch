e63c32390bccfb496e2ec660c037ad762e081783
Bohdan Shtepan <winsatana@ya.ru>
2016-03-30 04:08:05 +0300

    Add tests project; Empty for now.

---
+++ ModernDev.InTouch.Tests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+
+namespace ModernDev.InTouch.Tests
+{
+    [TestFixture]
+    public class BasicValuesTests
+    {
+        private InTouch _apiCLient;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When("http://localost/api/user/*").Respond("application/json", "{'name' : 'Test McGee'}");
+
+            _apiCLient = new InTouch(mockHttp, 1488, "superSecret");
+        }
+
+        [Test]
+        public void TestBaseValues()
+        {
+            Assert.That(_apiCLient.Session, Is.Null);
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            _apiCLient.Dispose();
+        }
+    }
+}
