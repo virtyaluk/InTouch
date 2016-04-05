bf9600a5564c3cf5afbb9809f8b597b89a7847b1
Bohdan Shtepan <winsatana@ya.ru>
2016-04-05 03:48:31 +0300

    Add MarketCreateCommentParams class.

---
+++ ModernDev.InTouch.Shared/API/MethodsParams/MarketCreateCommentParams.cs
@@ -0,0 +1,26 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// An <see cref="MarketCreateCommentParams"/> class describes a <see cref="MarketMethods.CreateComment"/> method params.
+    /// </summary>
+    public class MarketCreateCommentParams : BaseAddCommentParams
+    {
+        /// <summary>
+        /// Item Id.
+        /// </summary>
+        [MethodParam(Name = "item_id", IsRequired = true)]
+        public int ItemId { get; set; }
+    }
+}
