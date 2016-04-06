7905f5de360a30329169ca7d5739a90298a51219
Bohdan Shtepan <winsatana@ya.ru>
2016-04-06 03:56:39 +0300

    Add MarketEditParams class.

---
+++ ModernDev.InTouch.Shared/API/MethodsParams/MarketEditParams.cs
@@ -0,0 +1,22 @@
+/**
+ * This file\code is part of InTouch project.
+ *
+ * InTouch - is a .NET wrapper for the vk.com API.
+ * https://github.com/virtyaluk/InTouch
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+namespace ModernDev.InTouch
+{
+    /// <summary>
+    /// A <see cref="MarketSearchParams"/> class describes a <see cref="MarketMethods.Edit"/> method params.
+    /// </summary>
+    public class MarketEditParams : BaseMarketAddEditParams
+    {
+
+    }
+}
