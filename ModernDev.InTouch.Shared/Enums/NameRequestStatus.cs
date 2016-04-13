7c2a41361c6af85c9ef42322e48713ce21103147
Bohdan Shtepan <winsatana@ya.ru>
2016-04-13 03:05:07 +0300

    Add additional enum members; Add docs; Refactoring.

--- ModernDev.InTouch.Shared/Enums/NameRequestStatus.cs
+++ ModernDev.InTouch.Shared/Enums/NameRequestStatus.cs
@@ -10,6 +10,8 @@
  * Licensed under the GPLv3 license.
  */
 
+using System.Runtime.Serialization;
+
 namespace ModernDev.InTouch
 {
     /// <summary>
@@ -17,7 +19,34 @@ namespace ModernDev.InTouch
     /// </summary>
     public enum NameRequestStatus
     {
+        /// <summary>
+        /// The request is processing now.
+        /// </summary>
+        [EnumMember(Value = "processing")]
         Processing,
-        Declined
+
+        /// <summary>
+        /// The process is declined.
+        /// </summary>
+        [EnumMember(Value = "declined")]
+        Declined,
+
+        /// <summary>
+        /// The request is successfully accepted.
+        /// </summary>
+        [EnumMember(Value = "success")]
+        Success,
+
+        /// <summary>
+        /// There was a successful request recently, new request can't be sent before the date from the <see cref="NameRequest.RepeatDate"/> field.
+        /// </summary>
+        [EnumMember(Value = "was_accepted")]
+        WasAccepted,
+
+        /// <summary>
+        /// There was an unsuccessful request recently, new request can't be sent before the date from the <see cref="NameRequest.RepeatDate"/> field.
+        /// </summary>
+        [EnumMember(Value = "was_declined")]
+        WasDeclined
     }
 }
