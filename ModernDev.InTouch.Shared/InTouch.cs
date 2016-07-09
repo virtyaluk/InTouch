8e1116e980ae7df3422061a4d152f4cde027b68a
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-07-10 00:12:34 +0700

    APIVersion: type changed to string

--- ModernDev.InTouch.Shared/InTouch.cs
+++ ModernDev.InTouch.Shared/InTouch.cs
@@ -61,7 +61,7 @@ namespace ModernDev.InTouch
         /// <summary>
         /// The used API version.
         /// </summary>
-        public const double APIVersion = 5.52;
+        public const string APIVersion = "5.52";
 
         /// <summary>
         /// Determines the language for the data to be displayed on.
